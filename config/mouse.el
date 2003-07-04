4f80ba2e760e4f99330d4b673cd7251caa42b6a0
flepied <flepied>
2003-07-04 13:28:05 +0000

    oops inversion

--- config/mouse.el
+++ config/mouse.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Frederic Lepied <Frederic.Lepied@sugix.frmug.org>
 ;; Maintainer: Frederic Lepied <Frederic.Lepied@sugix.frmug.org>
-;; Version: $Id: mouse.el,v 1.2 2003-07-04 13:20:02 flepied Exp $
+;; Version: $Id: mouse.el,v 1.3 2003-07-04 13:28:05 flepied Exp $
 ;; Keywords: 
 ;;
 
@@ -62,7 +62,7 @@ To test this function, evaluate:
 (global-set-key [M-S-down-mouse-1] 'moving-mouse-drag-secondary)
 (global-set-key [down-mouse-2] 'mouse-drag-throw)
 
-(global-set-key [mouse-4] 'scroll-up)
-(global-set-key [mouse-5] 'scroll-down)
+(global-set-key [mouse-4] 'scroll-down)
+(global-set-key [mouse-5] 'scroll-up)
 
 ;;; 10mouse.el ends here
