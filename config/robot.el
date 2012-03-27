fc8ab502f2563d1e7cf42a070fbb0042d0514e9a
Frédéric Lepied <flepied@gmail.com>
2012-03-27 07:56:11 +0200

    add robotframework

---
+++ config/robot.el
@@ -0,0 +1,50 @@
+;;---------------------------------------------------------------
+;; Project         : config
+;; File            : robot.el
+;; Copyright       : (C) 2011 by 
+;; Author          : Frederic Lepied
+;; Created On      : Thu Jul 28 11:01:16 2011
+;; Purpose         : 
+;;---------------------------------------------------------------
+
+;;=============================================================================
+;; Autoloaded section.
+;;=============================================================================
+
+
+;;;###
+
+;;;***
+
+;;;### (autoloads nil "robot-mode" "robot-mode.el" (20017 9430))
+;;; Generated autoloads from robot-mode.el
+
+(autoload (quote robot-mode) "robot-mode" "\
+Major mode for editing Robot Framework text files.
+
+This mode rebinds the following keys to new function:
+\\{robot-mode-map}
+In the table above <remap> <function> means that the function is bound to whatever 
+key <function> was bound previously. To see the actual key binding press enter on
+top of the bound function. 
+
+You can use \\[beginning-of-defun] to move to the beginning of the kw 
+the cursor point is at and \\[end-of-defun] to move to the end of the kw. 
+To select (i.e. put a region around) the whole kw definition press \\[mark-defun].
+ 
+Set indent-tabs-mode to non-nil to use tabs for indantation. If indent-tabs-mode is nil, 
+c-basic-offset defines the amount of spaces that are inserted when indenting." t nil)
+
+;;;***
+
+
+;;=============================================================================
+;; Configuration section.
+;;=============================================================================
+
+;;=============================================================================
+;; command line options
+;;=============================================================================
+
+
+;;; robot.el ends here
---
+++ robot-mode.el
@@ -0,0 +1,1 @@
+/home/fred/external/robot-mode/robot-mode.el
